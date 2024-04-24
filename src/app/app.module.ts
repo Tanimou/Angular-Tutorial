d70bfb6bb0d04a8378a0860ce5a6b591a82cdf3c
Tanimou Cisse <tanimou@live.fr>
2024-04-24 12:03:42 +0000

    Adding, Product Details, Product Alert and Cart components Implementing routing functionnality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,27 +7,30 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      // { path: '', component: ProductAlertsComponent },
-    ])
+      { path: "", component: ProductListComponent },
+      { path: "products/:productId", component: ProductDetailsComponent },
+      { path: "cart", component: CartComponent },
+    ]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
 
 
 /*
